41a33398849c2dee2dbede9d4673da7914197759
lmorgan2398 <lmorgan2398@gmail.com>
2024-07-17 10:36:51 -0500

    Create basic functions for calculator
    Created simple functions for add, subtract, multiply,
    and divide in the script to be further edited once more
    of the calculator is complete.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 console.log('Hello World!')
+
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+function subtract(num1, num2) {
+    return num1 - num2;
+};
+
+function multiply(num1, num2) {
+    return num1 * num2;
+};
+
+function divide(num1, num2) {
+    return num1 / num2;
+};
+
+console.log(add(3,6));
+
+console.log(divide(6,3));
+
+console.log(multiply(5,4));
+
+console.log(subtract (14,6));
